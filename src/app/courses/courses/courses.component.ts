4546edacec323d173014e622761383a95fec4e95
lucas <lucaston2323@gmail.com>
2024-12-27 11:59:39 -0300

    feat: tabela do angular material para listar cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'Front-End'}
+  ];
+  displayedColumns = ['name', 'category'];
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
